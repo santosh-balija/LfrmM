0ba9fa30d0a0eed694c01266957875ff12bdb335
Kalyan Kumar Kancharla <48640903+Kalyan457@users.noreply.github.com>
2020-06-20 23:41:10 -0500

    After Carousal

--- src/components/Login/LoginPage.js
+++ src/components/Login/LoginPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './LoginPage.css';
 import LfrmMImg from '../../assets/images/LfrmM.png';
 import Auxillary from '../../hoc/Auxillary';
+import CarouselComp from './Carousel';
 
 const loginPage = () => (
     <Auxillary>
@@ -13,20 +14,21 @@ const loginPage = () => (
             </div>
         </div>
         <div className={classes.container3}>
-            <h3 className={classes.quotes}>It's not that I'm so smart, it's just that I stay with problems longer.</h3>
+            <CarouselComp />
         </div>
         <div className={classes.container2}>
             <div className={classes.dummyContainer}>
 
             </div>
             <div className={classes.leftDiv2} >
+                <br></br>
                 <h1>
-                    <span className={classes.greenFont}>Learn </span>
+                    <span className={classes.greenFontTitle}>Learn </span>
                     <sub className={classes.yellowFont1}>f</sub>
                     <span className={classes.yellowFont2}>rm </span>
                     <span className={classes.redFont}>Mistakes</span>
                 </h1>
-                <h2 style={{marginTop:'-17px'}}>
+                <h2 style={{marginTop:'-29px'}}>
                     Oops!! It is <span className={classes.greenFont}>Learn </span>
                                  <span className={classes.yellowFont2}>from </span>
                                  <span className={classes.redFont}>Mistakes</span>
@@ -44,6 +46,7 @@ const loginPage = () => (
                 <form>
                     <input className={classes.Input} type="text" name="name" placeholder="Email" />
                     <input className={classes.Input} type="email" name="email" placeholder="Password" />
+                    <a className={classes.links} href="#">Forgot Password</a>
                     <button className={classes.LoginBtn}>STEP IN TO LEARN</button>
                 </form>
             </div>
